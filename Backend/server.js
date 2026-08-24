5863644d396efe32e3d650166543008b83c236a0
Kelechi Emmanuel <ekelechi413@gmail.com>
2026-08-24 10:26:50 +0100

    ✅✅✅✅

--- Backend/server.js
+++ Backend/server.js
@@ -1,27 +1,29 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
+require('dotenv').config();
 
-app.use(express.json())
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
 
 app.use(cors({
     origin: [
         "http://localhost:5173",
         "http://localhost:5174",
-        "https://booking-app-seven-rosy.vercel.app/"
+        "https://booking-app-seven-rosy.vercel.app"
     ],
     credentials: true
 }));
 
-app.use('/api/auth', require('./Routes/authRoute'))
-app.use('/api/onboarding', require('./Routes/onboardingRoute'))
-app.use('/api/availability', require('./Routes/availabilityRoute'))
-app.use('/api/bookings', require('./Routes/bookingsRoute'))
-app.use('/api/dashboard', require('./Routes/dashboardRoute'))
+app.use('/api/auth', require('./Routes/authRoute'));
+app.use('/api/onboarding', require('./Routes/onboardingRoute'));
+app.use('/api/availability', require('./Routes/availabilityRoute'));
+app.use('/api/bookings', require('./Routes/bookingsRoute'));
+app.use('/api/dashboard', require('./Routes/dashboardRoute'));
 
-const PORT = process.env.PORT || 4999
+const PORT = process.env.PORT || 4999;
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-
-})
+});
